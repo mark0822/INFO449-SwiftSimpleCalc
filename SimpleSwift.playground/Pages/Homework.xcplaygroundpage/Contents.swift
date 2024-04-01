d7d94570baeacbfa535dc35f0bdcbbb02f0d1e02
mark0822 <mark20020822@gmail.com>
2024-04-01 16:21:07 -0700

    finished SimpleCalc

--- SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
+++ SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
@@ -1,40 +1,68 @@
 //: # Welcome to the UW Calculator Playground (Simple Version)
 //:
 print("Welcome to the UW Calculator Playground")
-//: This homework is designed to force you to exercise your knowledge of the Swift programming language. This homework does not involve iOS in any way. It uses the Playground feature of XCode to allow you to interactively write Swift code--the compiler will constantly check your code in the background.
-//:
-//: In this exercise, you will implement a pair of functions that do some simple mathematical calculations.
-//:
-//: ## Your tasks
-//: You are to implement two `calculate` functions, each of which take `Strings` that must be converted to integer values in order to perform the calculations intended. This is designed to make you comfortable with converting Strings to other values--a common task in mobile applications, when obtaining input from the user--for further processing. One of the functions takes an array of Strings, expecting each "part" of the calculation expression to be each be in its own String (such as "2" "+" "2"), and the second expects a single String containing the entire expression ("2 + 2").
-//: 
-//: You should make sure your calculate method can handle the following kinds of input:
-//: 
-//: * `calculate(["2", "+", "2"])`: This should return 2+2, or 4
-//: * `calculate(["2", "-", "2"])`: This should return 2-2, or 0
-//: * `calculate(["2", "*", "2"])`: This should return 2*2, or 4
-//: * `calculate(["2", "/", "2"])`: This should return 2/2, or 1
-//: * `calculate(["2", "%", "2"])`: This should return 2%2, or 0
-//: 
-//: > For those who aren't aware of it, the "%" operator is called the "modulo" operator, and it is the "remainder" result in an integer division that does not divide equally. Thus, `5 % 2` is 1 (5 divided by 2 is 2 remainder 1), `10 % 3` is 1 (10 divided by 3 is 3 remainder 1) and `4 % 2` is 0 (4 divided by 2 is 2 remainder 0).
-//: 
-//: The `calculate` method also needs to support a few other less-traditional expressions as well:
-//: 
-//: * `calculate(["1", "2", "3", "4", "5", "count"])`: This should return a count of all the number arguments, which in this case will be 5.
-//: * `calculate(["1", "3", "2", "avg"])`: This should return the average of the numbers, which is all of the values added up (1 + 3 + 2) and divided by the number of arguments (3).
-//: * `calculate(["5", "fact"])`: This should calculate the factorial of the single number passed in, which is that number multiplied by each number below it. 5-factorial is 5 * 4 * 3 * 2 * 1, or 120.
-//: 
-//: For this latter set of operations, it is safe to assume that `["count"]` (with no additional arguments) is 0, `["avg"]` is also 0, and `["fact"]` is 0. `["1", "fact"]` should return 1, and `["0", "fact"]` should also return 1. (Yes, 0-factorial is 1. True story.)
-//: 
+
+
+import Foundation
 func calculate(_ args: [String]) -> Int {
-    
-    return -1
+    var oper:String? = nil
+    var total:Int = 0
+    var count = 0
+    for item in args{
+        if Int(item) != nil{
+            guard var currentNum = Int(item) else { return -1 }
+            if oper == nil{
+                total += currentNum
+            }else{
+                switch oper{
+                case "+":
+                    total += currentNum
+                case "-":
+                    total -= currentNum
+                case "*":
+                    total *= currentNum
+                case "/":
+                    total /= currentNum
+                case "%":
+                    total %= currentNum
+                default:
+                    return -1
+                }
+            }
+        }else{
+            switch item {
+              case "count":
+                return count
+              case "avg":
+                if count == 0{
+                    return 0
+                }
+                return total/count;
+              case "fact":
+                if count == 0 {
+                    return 0
+                }
+                if total == 0 || total == 1 {
+                    return 1
+                }
+                var ans = 1
+                for num in 1...total{
+                        ans *= num
+                }
+                return ans
+              default:
+                oper = item
+            }
+        }
+        count+=1
+    }
+    return total
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    let argsArr = arg.components(separatedBy: " ")
+    return calculate(argsArr)
 }
-
 //: Below this are the test expressions/calls to verify if your code is correct.
 //:
 //: ***DO NOT MODIFY ANYTHING BELOW THIS***
@@ -86,7 +114,7 @@ calculate("5 fact") == 120
 //: Implement `calculate([String])` and `calculate(String)` to handle negative numbers. You need only make the tests below pass. (You do not need to worry about "fact"/factorial with negative numbers, for example.)
 //:
 //: This is worth 1 pt
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -101,25 +129,68 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
-//: Implement `calculate([String])` and `calculate(String)` to use 
-//: and return floating-point values. You need only make the tests 
-//: below pass. (Factorial of floating-point numbers doesn't make 
-//: much sense, either.)
-//:
-//: Swift *will* allow you to overload based on return types, so 
-//: the below functions can co-exist simultaneously with the 
-//: Integer-based versions above.
-//: 
-//: This is worth 1 pt
-/*
-func calculate(_ args: [String]) -> Double {
-    return -1.0
-}
-func calculate(_ arg: String) -> Double {
-    return -1.0
-}
+
+ func calculate(_ args: [String]) -> Double {
+     var oper:String? = nil
+     var total:Double = 0.0
+     var count = 0.0
+     for item in args{
+         if Double(item) != nil{
+             guard var currentNum = Double(item) else { return -1 }
+             if oper == nil{
+                 total += currentNum
+             }else{
+                 switch oper{
+                 case "+":
+                     total += currentNum
+                 case "-":
+                     total -= currentNum
+                 case "*":
+                     total *= currentNum
+                 case "/":
+                     total /= currentNum
+                 case "%":
+                     total.formTruncatingRemainder(dividingBy: currentNum)
+                 default:
+                     return -1
+                 }
+             }
+         }else{
+             switch item {
+               case "count":
+                 return count
+               case "avg":
+                 if count == 0{
+                     return 0.0
+                 }
+                 return total/count;
+               case "fact":
+                 if count == 0.0 {
+                     return 0.0
+                 }
+                 if total == 0.0 || total == 1.0 {
+                     return 1.0
+                 }
+                 var ans = 1.0
+                 for num in 1...Int(total){
+                         ans *= Double(num)
+                 }
+                 return ans
+               default:
+                 oper = item
+             }
+         }
+         count+=1
+     }
+     return total
+ }
+
+ func calculate(_ arg: String) -> Double {
+     let argsArr = arg.components(separatedBy: " ")
+     return calculate(argsArr)
+ }
 
 calculate(["2.0", "+", "2.0"]) == 4.0
 calculate([".5", "+", "1.5"]) == 2.0
@@ -128,4 +199,4 @@ calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
 calculate("1.0 2.0 3.0 4.0 5.0 count") == 5.0
-*/
+
